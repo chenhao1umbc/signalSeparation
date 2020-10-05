16ddc58d41a5ad834141eeb73eca0db44e79593f
chenhao1umbc <chenhao1@umbc.edu>
2020-10-05 16:54:38 -0400

    small changes to understanding zeyu code

--- debug.py
+++ debug.py
@@ -1,13 +1,12 @@
 #%%
-import torch
-import pickle
-file = open('/home/chenhao1/zeyu/datasets/dataset_0426_14000_128x20/train_set.pickle','rb')
-d = pickle.load(file)
-d0 = d.dataset.dataset.mixture_list[0]['mixture']
-l0 = d.dataset.dataset.mixture_list[0]['label']
-
-# %%
 from utils import *
+import pickle
+file = open('/home/chenhao1/zeyu/datasets/dataset_0426_14000_128x20//component/Blt.mat.pickle','rb')
+d = pickle.load(file)
+
+plt.imshow(d[0].T, interpolation='None', aspect='auto')
+plt.colorbar()
+
 
 # %%
 from scipy.signal import stft
