2fd4335e315b32027750bf208bc38b90ae3a5d8d
chenhao1umbc <chenhao1@umbc.edu>
2020-12-22 17:44:20 -0500

    fixed bug of aoa gen

--- debug.py
+++ debug.py
@@ -12,8 +12,8 @@ mix, l_mix = get_mixdata_label(mix=n_sources, pre='train_200_')
 "Mixture for the EM"
 db = 10  # db in [0, 20]
 power_ratio = 10**(-1*db/20)
-x = sources[1, n]*power_ratio + sources[2, n] + sources[3, n]
-# x = mix[n_comb,n]  # mixture of 6 components without power diff.
+# x = sources[1, n]*power_ratio + sources[2, n] + sources[3, n]
+x = mix[n_comb,n]  # mixture of 6 components without power diff.
 plot_x(x, title='Input mixture')  # plot input
 
 s_stft = torch.zeros(6, 200, 200)
@@ -58,22 +58,23 @@ n_c = 2  # 2 channels
 mse = []
 var_name = ['ble', 'bt', 'fhss1', 'fhss2', 'wifi1', 'wifi2']
 
-which_source = torch.tensor([0,1])
+which_source = torch.tensor([1,2])
 x = sources[which_source, n].sum(0)
 gt_stft = torch.rand(which_source.shape[0],200, 200, n_c, dtype=torch.complex64)
 for i in range(which_source.shape[0]):
-    s = sources[which_source[i], n]*torch.exp(torch.tensor(1j*pi*i/12))
+    s = sources[which_source[i], n]
     gt_stft[i, ... , 0] = st_ft(s)
-    gt_stft[i, ... , 1] = st_ft(s)*e**(1j*pi/20) # awgn(st_ft(s), snr=20)
+    gt_stft[i, ... , 1] = st_ft(s*e**(1j*pi/12*(i+1))) # awgn(st_ft(s), snr=20)
 
 init = awgn(s_stft[which_source], snr=200) #  gt_stft.abs().log()
 cjh_list, likelihood = em10(init_stft=init, stft_mix=gt_stft.sum(0), n_iter=n_iter) 
 
-for i in range(n_iter+1):
-    mse.append((((cjh_list[i] - gt_stft).abs()**2).sum()).item())
+for i in [0,1,2,3,30,50,90]:
     for ii in range(which_source.shape[0]):
         plot_x(cjh_list[i][ii,...,0], title=f'{var_name[which_source[ii]]} iter {i}')
-    plt.close('all')
+
+for i in range(n_iter+1):
+    mse.append((((cjh_list[i] - gt_stft).abs()**2).sum()).item())   
 plt.figure()
 plt.plot(mse, '-x')
 plt.figure()
